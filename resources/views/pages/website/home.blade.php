7ee7cf993edd80f0c9d748332f0c2a259ac928c2
Montashir Billah <montashir92@gmail.com>
2022-05-29 14:45:42 +0600

    About Image Resize

--- resources/views/pages/website/home.blade.php
+++ resources/views/pages/website/home.blade.php
@@ -1,5 +1,5 @@
 @extends('layouts.master_home')
-      @section('web_content')
+@section('web_content')
 
       <section id="slider">
         <div id="main-slide" class="carousel slide" data-ride="carousel">
@@ -80,7 +80,7 @@
                     @endif
                 </div>
                 <div class="col-md-6 col-lg-6 col-xs-12 wow fadeInRight" data-wow-delay="0.3s">
-                    <img class="img-fluid" src="{{ asset('img/about/' . $setting->about_image) }}" alt="">
+                    <img class="img-fluid about_img" src="{{ asset('img/about/' . $setting->about_image) }}" alt="">
                 </div>
             </div>
         </div>
